0dcb93fa71cfcfde6991bbb82a48819df6a0307f
ryan <rymon23@gmail.com>
2023-05-22 14:18:28 -0400

    started build_move_tree

--- knights_travails.rb
+++ knights_travails.rb
@@ -14,44 +14,31 @@ class KnightPathFinder
         @root_node = PolyTreeNode.new(start_pos)
     end
 
-    def build_move_tree(self.root_node)
-        (0...8).each do |row_i|
-            (0...8).each do |col_i|
-                if self.valid_moves([row_i,col_i]) && !root_node.children.include?(pos)
-                root_node.children << [row_i, col_i]
-                root_node = [row_i, col_i]
-                end
-            end
-        end
-        
+    def build_move_tree(root_node)
+        until new_move_positions(root_node).empty?
+            root_node.children << new_move_positions(root_node).shift
+            root_node.children.each do |child|  
+                build_move_tree(child)
+            end 
+        end 
     end
 
     def self.valid_moves(pos)
-            root_x , root_y = start_pos[0] , start_pos[1]
+        valid_moves = []
             x, y = pos[0], pos[1]
-            unless (0..7).include?(x) && (0..7).include?(y)
-                return false
+            [1, -1].each do |ele1|
+                [2, -2].each do |ele2|
+                    valid_moves << [(x + ele1), (y + ele2)]
+                    valid_moves << [(x + ele2), (y + ele1)]
+                end 
             end 
-            if x-(root_x) == abs(1) && y-(root_y) == abs(2)
-                return true 
-            elsif   
-                x-(root_x) == abs(2) && y-(root_y) == abs(1)
-                return true
-            else 
-                return false 
-            end 
+          valid_moves.select do |newpos|
+            (0..7).include?(newpos[0]) && (0..7).include?(newpos[1])
+          end 
     end
 
     def new_move_positions(pos)
-        unless considered_positions.include?(pos)
-            @considered_positions << pos if KnightPathFinder.valid_moves(pos)
-            return pos
-        end 
+        return @considered_positions += KnightPathFinder.valid_moves(pos).select { |validpos| !considered_positions.include?(validpos)} 
     end 
-
-
-
-
-
 
 end
